dfea917af0a2925a727cba5d14ef65eaacd6d432
Ali Elahi <alielahi.1378@gmail.com>
2021-08-09 12:43:59 +0430

    Feature/gender card and reusable card are added to the project

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'gender_card.dart';
+import 'reusable_card.dart';
+
+const double bottomContainerWidth = 80.0;
+const Color activeCardColor = Color(0xFF1D1E33);
+const Color bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,13 +17,66 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        brightness: Brightness.dark,
-        title: Center(child: Text('BMI CALCULATOR')),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-    );
+        appBar: AppBar(
+          brightness: Brightness.dark,
+          title: Center(
+            child: Text('BMI CALCULATOR'),
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                      cardChild: GenderCard(
+                        label: 'Male',
+                        icon: FontAwesomeIcons.mars,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                      cardChild: GenderCard(
+                        label: 'Female',
+                        icon: FontAwesomeIcons.venus,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColor,
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              height: bottomContainerWidth,
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 10.0),
+            )
+          ],
+        ));
   }
 }

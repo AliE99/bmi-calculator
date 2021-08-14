d6cee2f471857fb67b3582ca355dd0cf10572af6
Ali Elahi <alielahi.1378@gmail.com>
2021-08-14 11:09:34 +0430

    Feature/ Designing Result page. appbar and body are created so far;

--- lib/constants.dart
+++ lib/constants.dart
@@ -19,3 +19,23 @@ const kLargeButtonTextStyle = TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
 );
+
+const kResultPageTitleStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMINumberTextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMIBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,7 @@
+import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
+
+import 'reusable_card.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({Key? key}) : super(key: key);
@@ -6,10 +9,58 @@ class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Results Page'),
-      ),
-      body: Text('Hello'),
-    );
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                'Your Result',
+                style: kResultPageTitleStyle,
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: ReusableCard(
+                colour: kActiveCardColor,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Normal',
+                      style: kResultTextStyle,
+                    ),
+                    Text(
+                      '24.1',
+                      style: kBMINumberTextStyle,
+                    ),
+                    Text(
+                      'your BMI is quite high',
+                      textAlign: TextAlign.center,
+                      style: kBMIBodyTextStyle,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            // Expanded(
+            //   child: Container(
+            //     child: Center(
+            //       child: Text(
+            //         'Re-Calculate',
+            //         style: kLargeButtonTextStyle,
+            //       ),
+            //     ),
+            //     color: kBottomContainerColor,
+            //     height: kBottomContainerWidth,
+            //     width: double.infinity,
+            //   ),
+            // )
+          ],
+        ));
   }
 }
